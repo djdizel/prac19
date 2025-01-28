6b9a63b4a545d1e9ef4a3de3c82cdc9ad7618d33
djdizel <158520404+djdizel@users.noreply.github.com>
2025-01-28 14:36:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Books myBook = new Books("Война и мир", "Лев Толстой", 1869, 1225);
-        
         myBook.Info();
+        Console.ReadLine();
     }
 }
